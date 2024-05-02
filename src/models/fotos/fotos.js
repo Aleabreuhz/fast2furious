a3b04bea30b957eb752612ae47bcba3a1499bb80
Aleabreuhz <145705520+Aleabreuhz@users.noreply.github.com>
2024-05-03 00:41:26 +0100

    Add files via upload

--- src/models/fotos/fotos.js
+++ src/models/fotos/fotos.js
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
+
 let fotos = [
     {
-        src: "fast-and-furious-movies-best-cars-ranked.jpeg"
-    }  
-    
+        title: "Para los rapidos y furiosos",
+        preview: "fast2.jpg",
+        content: "Bienvenido a la pagina de Fast and Furious la saga mas famosa del cine."
+    },
+    {
+        title: "Musica",
+        preview: "toretoduro.jpg",
+        content: "Escucha el nuevo tema de El Alfa CORONOW junto a artistas famosos y la participación de Vin Diesel.",
+        link:"https://www.youtube.com/watch?v=rZeJGPJV3fA&ab_channel=ElAlfaElJefeTV"
+    },
+    {
+        title: "Conócelos",
+        preview: "paulwalkersupra.jpg",
+        content: "Conoce a nuestros personajes mas famosos en nuestra pagina.",
+    },
+    {
+        title: "¡El sitio me suena!",
+        preview: "ff5.jpg",
+        content: "Conoce los lugares mas emblematicos de la saga",
+    },
 ];
 
 export default fotos;
